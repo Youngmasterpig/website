d4130071ca0f84b11ccf8e4dc538aa3319f8da41
unknown <FB481738@fb.is>
2018-04-24 14:58:52 +0000

    Initial commit

---
+++ music/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from .models import Album
+
+
+def index(request):
+    all_albums = Album.objects.all()
+    html = ""
+    for album in all_albums:
+
+    return HttpResponse(html)
+
+def details(request, album_id):
+    return HttpResponse("<h2>Details for Album id: " + str(album_id) + "</h2>")
