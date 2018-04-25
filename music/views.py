7f3043aa94230426e436d6996b180d5d843a7bdc
mikael <Mikael.jon.adils@gmail.com>
2018-04-25 10:09:51 +0000

    test

--- music/views.py
+++ music/views.py
@@ -4,12 +4,13 @@ from .models import Album
 
 def index(request):
     all_albums = Album.objects.all()
-    html = ""
+    html = ''
     for album in all_albums:
-
+        url = '/music/' + str(album.id) + '/'
+        html += '<a href="' + url +'">' + album.album_title + '</a><br>'
     return HttpResponse(html)
+
 
 def details(request, album_id):
     return HttpResponse("<h2>Details for Album id: " + str(album_id) + "</h2>")
 
-nibbs
