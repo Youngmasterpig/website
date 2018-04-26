50ca6b067f59ee318a7c1b122f146f25d6f0f98c
mikael <Mikael.jon.adils@gmail.com>
2018-04-26 10:19:14 +0000

    test

--- music/models.py
+++ music/models.py
@@ -15,3 +15,6 @@ class Song(models.Model):
     Album = models.ForeignKey(Album, on_delete=models.CASCADE)
     file_type = models.CharField(max_length=10)
     song_title = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.song_title
