80dcece4ee86024920d9a1b94d50ed940aeebf2b
mikael <Mikael.jon.adils@gmail.com>
2018-05-09 10:42:56 +0000

    test

--- music/models.py
+++ music/models.py
@@ -1,22 +1,49 @@
 from django.db import models
 
+from django.urls import reverse
+
+
+
+
 
 class Album(models.Model):
+
     artist = models.CharField(max_length=250)
+
     album_title = models.CharField(max_length=500)
+
     genre = models.CharField(max_length=100)
-    album_logo = models.CharField(max_length=1000)
+
+    album_logo = models.FileField()
+
+
+
+    def get_absolute_url(self):
+
+        return reverse('music:details', kwargs={'pk': self.pk})
+
+
 
     def __str__(self):
+
         return self.album_title + ' - ' + self.artist
 
 
+
+
+
 class Song(models.Model):
-    Album = models.ForeignKey(Album, on_delete=models.CASCADE)
+
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+
     file_type = models.CharField(max_length=10)
-    song_title = models.CharField(max_length=250)
+
+    song_titles = models.CharField(max_length=250)
+
     is_favorite = models.BooleanField(default=False)
 
 
+
     def __str__(self):
-        return self.song_title
+
+        return self.song_titles
